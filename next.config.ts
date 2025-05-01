d1e7f033c120153734976d586c532e68bc2cfcb9
Leonardo Cech <cech@5cycle.com.br>
2025-05-01 17:54:27 -0300

    Next config for domain

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // Necessário para exportação estática
-  },
-  basePath: '/leonardo-cech-website',
-  assetPrefix: '/leonardo-cech-website/',
+  }
 };
 
 export default nextConfig;
